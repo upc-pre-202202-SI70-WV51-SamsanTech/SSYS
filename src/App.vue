00585d69604e46e3dfad5982451c5c7fcd3266dd
JackDelAguila <jackdelaguilavilla03@gmail.com>
2022-09-27 19:09:53 -0500

    hotfix(main.css):Delete general styles

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,31 @@
 <script setup>
-import Workers from './pages/auth/workers.vue';
-
+  import { RouterLink, RouterView } from 'vue-router';
 </script>
 
 <template>
-  <Workers></Workers>
+  <div class="navbar">
+    <div class="navbar__links">
+      <RouterLink to="/dashboard">Dashboard</RouterLink>
+      <RouterLink to="/workers">Workers</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </div>
+  </div>
+
+  <router-view/>
 </template>
 
 <style scoped>
+/*navbar div class navbar*/
+.navbar {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1rem;
+  background-color: #fff;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+}
 
+img {
+    width: 70px;
+  }
 </style>
