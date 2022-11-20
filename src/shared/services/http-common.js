d2666c9f357daf9a9117d9ff9b08ad9cf707c00f
Breydi Ramos Terrones <ogreydi123@gmail.com>
2022-11-20 13:56:57 -0500

    feat(hcm): back end connect with hcm functionalities.

--- src/shared/services/http-common.js
+++ src/shared/services/http-common.js
@@ -1,6 +1,27 @@
-import axios from 'axios';
+import axios from "axios";
 
-export default axios.create({
-   baseURL: 'https://localhost:7171/api/v1',
-   headers: { 'Content-type': 'application/json' }
+const instance = axios.create({
+  baseURL: "https://localhost:7171/api/v1",
+  headers: {
+    "content-type": "application/json",
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET,PUT,POST,DELETE,PATCH,OPTIONS",
+  },
 });
+
+instance.interceptors.request.use(
+  (config) => {
+    if (
+      !config.url?.includes("users/auth/sign-in") &&
+      !config.url?.includes("users/auth/sign-up") &&
+      config.headers
+    )
+      config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
+  }
+);
+
+export default instance;
